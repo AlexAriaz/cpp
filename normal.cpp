97392c8b2ee15a3d63198b01edef51b5ee248b26
Alex <alexariaz@gmail.com>
2017-05-19 13:53:31 -0700

    Hello World

---
+++ normal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+main(){
+int mod=60%30;
+if (60%3==0)
+{cout<<"works\n";}
+else{cout<<"doesnt work\n";}
+
+return 0;
+}
+
+//unsigned long normalise(unsigned long input_time){
+	
+//}
+
